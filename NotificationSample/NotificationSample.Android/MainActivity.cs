e1f8101709f0885093b048cbf0616efacee40152
sidhuraj <mr.prvsantosh@rediffmail.com>
2020-04-06 18:34:49 +0530

    first commit

---
+++ NotificationSample/NotificationSample.Android/MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Android.Gms.Common;
+using Android.Content;
+using Xamarin.Forms;
+
+namespace NotificationSample.Droid
+{
+    [Activity(Label = "NotificationSample", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            bool flag = false;
+            if(Intent.Extras !=null)
+            {
+                flag = true;
+            }
+       
+            LoadApplication(new App(flag));
+            IsPlayServicesAvailable();
+        }
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    //msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                }
+
+                else
+                {
+                    // msgText.Text = "This device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                // do whatever if play service is not available
+                //msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+    }
+}
---
+++ NotificationSample/NotificationSample/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace NotificationSample
+{
+    public partial class App : Application
+    {
+        public App(bool shallNavigate)
+        {
+            InitializeComponent();
+            if(shallNavigate)
+            {
+                MainPage = new NavigationPage(new DashboardPage());
+            }
+            else
+            {
+                MainPage = new NavigationPage(new MainPage());
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
